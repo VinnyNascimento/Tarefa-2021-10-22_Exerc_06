3b22241ff240117e88af0e08e97dcd1fa997c7ee
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-11-12 03:00:04 -0300

    Add files via upload

---
+++ Exerc_06/Exerc_06/Exerc_06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exerc_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declarações & instâncias
+            Calculo s;
+            s = new Calculo();
+            #endregion
+
+            #region Introducao
+            Console.Write("Converta Milhas Marítimas em quilômetros (km)");
+            Console.ReadLine();
+            #endregion
+
+            #region entrada
+            Console.Write("Informe o valor em milha marítima: ");
+            s.setNum1(double.Parse(Console.ReadLine()));
+            #endregion
+
+            #region processo
+            s.calcular();
+            #endregion
+
+            #region saída
+            Console.WriteLine("O valor {0} em milhas marítimas é: {1} quilometros (km)", s.getNum1(), s.getResultado());
+            #endregion
+        }
+    }
+}
